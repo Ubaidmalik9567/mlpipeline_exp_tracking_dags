12271879835694d68fd19d85991901fafc523bf8
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-08-02 15:41:07 +0500

    add new feature in mlflow

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,12 +3,18 @@ import sys
 import pathlib
 import pickle
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score,confusion_matrix
 import json
 import logging
 import yaml
+import seaborn as sns
+import matplotlib.pyplot as plt
 import mlflow
+import dagshub
 
+dagshub.init(repo_owner= "Ubaidmalik9567", repo_name="mlpipeline_exp_tracking_dags", mlflow= True)
+mlflow.set_tracking_uri("https://github.com/Ubaidmalik9567/mlpipeline_exp_tracking_dags.git")
+mlflow.set_experiment(experiment_name="dagsdash hub Demo")
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -99,6 +105,24 @@ def main():
             for key, value in value.items():
                 mlflow.log_param(f'{param}_{key}', value)
 
+                
+        # Create a confusion matrix plot
+        plt.figure(figsize=(6, 6))
+        cf_matrix = confusion_matrix(x, y)
+        sns.heatmap(cf_matrix, annot=True, fmt='d', cmap='Blues')
+        plt.xlabel('Predicted')
+        plt.ylabel('Actual')
+        plt.title('Confusion Matrix')
+
+        # Save the necessary artifact
+        plt.savefig('confusion_matrix.png')
+
+        mlflow.log_artifact('confusion_matrix.png')
+        mlflow.log_artifact(__file__)
+        
+        mlflow.sklearn.log_model(model, "GradientBoostingClassifier")
+        mlflow.set_tag("autor", "Ubaid ur Rehman")
+
         logging.info("Main function completed successfully.")
     except Exception as e:
         logging.error(f"Error in main function: {e}")
@@ -106,67 +130,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-'''import pandas as pd
-import sys
-import pathlib
-import pickle
-import numpy as np
-from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score
-import json
-
-def load_and_split_data(dataset_path: pd.DataFrame)-> tuple:
-    dataset = pd.read_csv(dataset_path)
-    xtest = dataset.iloc[:,0:-1]
-    ytest = dataset.iloc[:,-1]
-
-    return xtest, ytest
-
-def load_save_model(file_path):
-     with open(file_path, 'rb') as file:
-            model = pickle.load(file)
-            return model
-
-
-def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
-        y_pred = clf.predict(X_test)
-        y_pred_proba = clf.predict_proba(X_test)[:, 1]
-
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-        auc = roc_auc_score(y_test, y_pred_proba)
-
-        metrics_dict = {
-            'accuracy': accuracy,
-            'precision': precision,
-            'recall': recall,
-            'auc': auc
-        }
-        return metrics_dict
-
-def save_metrics(metrics: dict, file_path: str) -> None:
-        with open(file_path + "/metrics.json", 'w') as file:
-            json.dump(metrics, file, indent=4)
-
-def main():
-    current_dir = pathlib.Path(__file__)
-    home_dir = current_dir.parent.parent.parent
-
-    path = sys.argv[1]
-    save_metrics_location = home_dir.as_posix() + "/reports"
-    
-    processed_datasets_path = home_dir.as_posix() + path + "/processed_testdata.csv"
-    trained_model_path = home_dir.as_posix() + "/models/model.pkl"
-    
-    x, y = load_and_split_data(processed_datasets_path)
-    model = load_save_model(trained_model_path)
-
-    metrics_dict = evaluate_model(model, x, y)
-    save_metrics(metrics_dict,save_metrics_location)
-
-
-
-if __name__ == "__main__":
-     main()'''
